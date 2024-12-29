9849e02171571a56e28679e8433e70e431a3efd0
saad1551 <saad.ashraf.9094@gmail.com>
2024-12-30 04:11:36 +0500

    add logout in header for logged in users

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { HeaderContainer, HeaderTitle, ButtonContainer, HeaderButton } from './SharedStyles';
 
 const Header = () => {
-  const loggedIn = localStorage.getItem("isLoggedIn");
+  const loggedIn = localStorage.getItem("isLoggedIn") === "true";
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    if (window.confirm("Are you sure you want to log out?")) {
+      localStorage.setItem("isLoggedIn", "false");
+      navigate("/login");
+    }
+  };
 
   return (
     <HeaderContainer>
@@ -13,6 +21,7 @@ const Header = () => {
           <>
             <Link to="/"><HeaderButton>Main Page</HeaderButton></Link>
             <Link to="/admin"><HeaderButton>Admin Portal</HeaderButton></Link>
+            <HeaderButton onClick={handleLogout}>Logout</HeaderButton>
           </>
         ) : (
           <>
